b0ae4a1c13386fce43bf61dd8abeb4209e4d9946
Farid <farid-n@auto.kz>
2021-01-12 19:12:26 +0600

    Added Test for incorrect Login data

--- tests/Functional/ProxyTest.php
+++ tests/Functional/ProxyTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
+use Exception;
 use Proxy\OAuth\Model\Access\Type\PasswordType;
 use Proxy\OAuth\Model\Access\Type\UsernameType;
 use Proxy\OAuth\Proxy;
@@ -15,7 +16,7 @@ class ProxyTest extends WebTestCase
     public function testLoginSuccess(): void
     {
         $username = 'login';
-        $password = 'login';
+        $password = 'password';
 
         $jwt = $this->converter->fromFrontendToJWT($this->login($username, $password));
 
@@ -24,6 +25,18 @@ class ProxyTest extends WebTestCase
         self::assertArrayHasKey('expires_in', $jwt);
         self::assertArrayHasKey('access_token', $jwt);
         self::assertArrayHasKey('refresh_token', $jwt);
+    }
+
+    public function testLoginEmptyData(): void
+    {
+        $username = '__INCORRECT__USERNAME__';
+        $password = '__INCORRECT__PASSWORD__';
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('The User Entity not found, check user, domain credentials.');
+        $this->expectExceptionCode(400);
+
+        $this->login($username, $password);
     }
 
     private function login(string $login, string $password): string
