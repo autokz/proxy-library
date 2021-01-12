72e1c773f45976a09df8569bd2f86508b1a218fd
Farid <farid-n@auto.kz>
2021-01-12 11:50:02 +0600

    Mod AccessAction for work with Handler

--- src/Action/AccessAction.php
+++ src/Action/AccessAction.php
@@ -4,12 +4,28 @@ declare(strict_types=1);
 
 namespace Proxy\OAuth\Action;
 
-use Proxy\OAuth\Helpers\GuzzleHttpClient;
-use Proxy\OAuth\Interfaces\ConfigStoreInterface;
-use Proxy\OAuth\Interfaces\ConverterInterface;
-use Proxy\OAuth\Interfaces\HttpClientInterface;
+use Proxy\OAuth\Model\Access\Command\Check\Command;
+use Proxy\OAuth\Model\Access\Command\Check\Handler;
+use Proxy\OAuth\Validator\Validator;
 
 class AccessAction
 {
+    private Handler $handler;
+    private Validator $validator;
 
+    public function __construct(Handler $handler, Validator $validator)
+    {
+        $this->validator = $validator;
+        $this->handler = $handler;
+    }
+
+    public function handle(string $OAuthData): void
+    {
+        $command = new Command();
+        $command->OAuthData = $OAuthData;
+
+        $this->validator->validate($command);
+
+        $this->handler->handle($command);
+    }
 }
--- src/Model/Access/Command/Check/Handler.php
+++ src/Model/Access/Command/Check/Handler.php
@@ -1,10 +1,60 @@
 <?php
 
-
 namespace Proxy\OAuth\Model\Access\Command\Check;
 
+use Proxy\OAuth\Action\RefreshAction;
+use Proxy\OAuth\Interfaces\ConfigStoreInterface;
+use Proxy\OAuth\Interfaces\ConverterInterface;
+use Proxy\OAuth\Interfaces\HttpClientInterface;
 
 class Handler
 {
+    private ConverterInterface $converter;
+    private ConfigStoreInterface $configStore;
+    private HttpClientInterface $httpClient;
+    private string $url;
 
+    public function __construct(
+        ConverterInterface $converter,
+        ConfigStoreInterface $configStore,
+        HttpClientInterface $httpClient
+    ) {
+        $this->converter = $converter;
+        $this->configStore = $configStore;
+        $this->httpClient = $httpClient;
+
+        $baseUrl = trim($this->configStore->get('OAUTH_BASE_URL'), '/');
+        $checkUrl = trim($this->configStore->get('OAUTH_CHECK_URL'), '/');
+
+        $this->url = $baseUrl . '/' . $checkUrl;
+    }
+
+    public function handle(Command $command): void
+    {
+        $OAuthData = $command->OAuthData;
+
+        $JWT = $this->converter->fromFrontendToJWT($OAuthData);
+
+        if (!$this->check($JWT)) {
+            $JWT = (new RefreshAction($this->configStore, $this->httpClient))
+                ->refresh($JWT);
+        }
+
+        $this->converter->fromJWTToFrontend($JWT);
+    }
+
+    private function check(?array $decryptedAuthData): bool
+    {
+        if (!$decryptedAuthData || isset($decryptedAuthData['access_token'])) {
+            return false;
+        }
+
+        $headers = [
+            'Authorization' => $this->configStore->get('OAUTH_TYPE') . ' ' . $decryptedAuthData['access_token'],
+        ];
+
+        $responseClient = $this->httpClient->get($this->url, [], $headers, ['http_errors' => false]);
+
+        return $responseClient->getStatusCode() === 200;
+    }
 }
